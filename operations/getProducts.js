573f7e6f9ee39fe1009dea2cf04aa020a9bd6d47
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-06-07 23:08:53 +0300

    try deploy

--- operations/getProducts.js
+++ operations/getProducts.js
@@ -3,8 +3,8 @@ import getBasePathForFetch from '@/helpers/getBasePathForFetch';
 const getProducts = async () => {
   try {
     const res = await fetch(`${getBasePathForFetch()}/api/products`);
-    const data = await res.json();
-    return data;
+    const data = await res.text();
+    return JSON.parse(data);
   } catch (error) {
     throw new Error(`Cant get data from DB ${error}`);
   }
