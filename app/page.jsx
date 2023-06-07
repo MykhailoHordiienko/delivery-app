2384a6ae547be42aec459dd51cd7ff75e8a80ff2
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-06-07 20:22:17 +0300

    try to deploy change fetch

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
 import ShoppingList from '@/components/ShoppingList/ShoppingList';
-import { getProducts } from '@/operations/getProducts';
+import getBasePathForFetch from '@/helpers/getBasePathForFetch';
+// import { getProducts } from '@/operations/getProducts';
 
 export default async function Home() {
-  const response = await getProducts();
+  //   const response = await getProducts();
+  const res = await fetch(`${getBasePathForFetch()}/api/products`);
+  const response = await res.json();
   const shops = [...Object.keys(...response)].slice(1);
 
   return (
