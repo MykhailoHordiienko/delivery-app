c9b4ef59be6b74e10821349f8ea427c1993d6451
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-06-07 20:45:28 +0300

    try to deploy add headers to fetch

--- operations/getProducts.js
+++ operations/getProducts.js
@@ -2,7 +2,12 @@ import getBasePathForFetch from '@/helpers/getBasePathForFetch';
 
 export const getProducts = async () => {
   try {
-    const res = await fetch(`${getBasePathForFetch()}/api/products`);
+    const res = await fetch(`${getBasePathForFetch()}/api/products`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     const data = await res.json();
     return data;
   } catch (error) {
