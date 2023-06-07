70cc1dd4bdb3d4c35c637372d68abb2a30573acc
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-06-07 22:16:11 +0300

    try

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -2,8 +2,8 @@ import { getProductsFromDb } from '@/db/mongodb/mongoOperations';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const res = await getProductsFromDb();
   try {
+    const res = await getProductsFromDb();
     return NextResponse.json(res);
   } catch (error) {
     throw new Error('ERROR IN ROUTE PRODUCTS');
