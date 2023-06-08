4aaa02674ea6c2cc16b4db8ad11bb22f15b0f514
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-06-08 11:31:22 +0300

    time out

--- app/shoppingcart/page.jsx
+++ app/shoppingcart/page.jsx
@@ -2,6 +2,7 @@
 import { useGlobalContext } from '@/app/Context/store';
 import CartList from '@/components/CartList/CartList';
 import FormCart from '@/components/FormCart/FormCart';
+import Link from 'next/link';
 
 const ShoppingCartPage = () => {
   const { order } = useGlobalContext();
@@ -16,7 +17,19 @@ const ShoppingCartPage = () => {
         <p className="mx-auto mt-9">Total Prise: {totalPrice}$</p>
       </aside>
       <section className="p-6 w-full overflow-x-scroll rounded-lg flex border border-black bg-zinc-100">
-        <CartList order={order} />
+        {order.length < 1 ? (
+          <div className="flex flex-col items-center justify-center gap-8 mx-auto">
+            <h1 className="text-6xl">Empty Cart</h1>
+            <Link
+              href="/"
+              className="text-3xl border border-black rounded-md p-3 hover:text-blue-600 focus:text-blue-600"
+            >
+              Shops
+            </Link>
+          </div>
+        ) : (
+          <CartList order={order} />
+        )}
       </section>
     </>
   );
